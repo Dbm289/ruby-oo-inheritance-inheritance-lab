37d93eb70385822318588c2900797c04bedd1a8c
Dan <DanielMooreRealPerson@gmail.com>
2020-12-20 22:33:40 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+    
+    def initialize
+        @KNOWLEDGE = []
+    end
+    
+
+    def learn(string)
+        @KNOWLEDGE << string
+
+    end
+
+    def knowledge 
+        @KNOWLEDGE
+
+    end
 
 end
